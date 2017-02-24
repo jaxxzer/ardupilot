0cbba7f30feb2f549bd20ea42365287baa03cd07
Jacob Walser <jwalser90@gmail.com>
2017-02-24 00:35:57 -0500

    Revert "AC_AttitudeControl: added support for AP_AHRS_View"
    This reverts commit 1345bf87377bd9dd788d22c5ec39f0a7d3d13742.

--- libraries/AC_AttitudeControl/AC_AttitudeControl_Heli.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl_Heli.h
@@ -30,7 +30,7 @@
 
 class AC_AttitudeControl_Heli : public AC_AttitudeControl {
 public:
-    AC_AttitudeControl_Heli( AP_AHRS_View &ahrs,
+    AC_AttitudeControl_Heli( AP_AHRS &ahrs,
                         const AP_Vehicle::MultiCopter &aparm,
                         AP_MotorsHeli& motors,
                         float dt) :
